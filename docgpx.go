83e5e2bbdbec57b4bf7c5725f0a70e743c7f487e
go-opt <38841942+go-opt@users.noreply.github.com>
2018-09-12 12:54:49 -0400

    Update docgpx.go

--- docgpx.go
+++ docgpx.go
@@ -2,7 +2,7 @@
 
 
 /*
-Package gpx ("Go for Cplex") provides an interface to a small subset of CPX 
+Package gpx ("linear programming Go for Cplex") provides an interface to a small subset of CPX 
 (C language) functions available in the CPLEX Callable Library. It is complementary to,
 but independent of the lpo ("linear programming object") package.
 
